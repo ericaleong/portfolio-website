4d7613bddfc37358c831b027b66578fbebbe51e1
ericaleong <59629488+ericaleong@users.noreply.github.com>
2020-03-02 19:45:40 -0700

    Update app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  res.writeHead(404, {'Content-Type': 'text/html'});
+  fs.createReadStream(__dirname + '/404.html').pipe(res)
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
+});
