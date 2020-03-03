176856beb09aacb00288822e033b5acf1730b696
ericaleong <59629488+ericaleong@users.noreply.github.com>
2020-03-03 13:10:57 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res, next){
-  res.writeHead(200, {'Content-Type': 'text/html'})
-  res.send('index.html');
-});
+// app.get('/', function(req, res, next){
+//   res.writeHead(200, {'Content-Type': 'text/html'})
+//   res.send('index.html');
+// });
+
+
+// app.get('/', function(req, res) {
+//   res.json(app.index);
+// })
+
 
 app.use(function(req, res, next) {
   res.writeHead(404, {'Content-Type': 'text/html'});
@@ -21,3 +27,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, function(){
   console.log(`Listening on port ${PORT}`);
 });
+
